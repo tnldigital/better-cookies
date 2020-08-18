8d5ac5a208b27552b25cf81101d0594cc430b261
Fraser Cook <github@thundrnlightnin.com>
2020-08-18 10:19:03 +0100

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ export default function MyApp({ Component, pageProps }) {
   	return (
 	  	<>
 			<Head>
-				<title>Next.js + Tailwind CSS</title>
+				<title>Recippy | No nonsense Recipes</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<Component {...pageProps} />
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	theme: {
+		container: {
+			screens: {
+				sm: "100%",
+				md: "100%",
+				lg: "600px",
+				xl: "900px"
+			},
+		},
+	},
+};
